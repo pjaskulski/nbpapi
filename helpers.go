ceb3c6d84201aef364c94c8ce87401cd0dd4aedb
piotr.jaskulski@gmail.com <piotr.jaskulski@gmail.com>
2020-12-02 14:02:16 +0100

    refactoring again

--- helpers.go
+++ helpers.go
@@ -3,53 +3,9 @@
 package nbpapi
 
 import (
-	"errors"
-	"io/ioutil"
 	"math/rand"
-	"net/http"
 	"time"
 )
-
-// getData - universal function that returns JSON/XML (or error)
-// based on the address provided
-func getData(address string, format string) ([]byte, error) {
-	if format == "json" {
-		format = "application/json"
-	} else if format == "xml" {
-		format = "application/xml"
-	}
-
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	req, err := http.NewRequest("GET", address, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Accept", format)
-
-	r, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Body.Close()
-
-	if r.StatusCode >= 400 {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			return nil, err
-		}
-		return nil, errors.New(string(body))
-	}
-
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
 
 // littleDelay - delay function, so as not to bother the NBP server too much...
 func littleDelay() {
--- nbpapi.go
+++ nbpapi.go
@@ -1,6 +1,59 @@
 package nbpapi
 
-// SetLang function (language for output functions)
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+/* getData - universal function that retrieves data from the NBP website
+   and returns them in the form of JSON / XML (or error), based on
+   the arguments provided:
+
+   url - NBP web api address
+   format - 'json' or 'xml'
+*/
+func getData(url string, format string) ([]byte, error) {
+	if format == "json" {
+		format = "application/json"
+	} else if format == "xml" {
+		format = "application/xml"
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", format)
+
+	r, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode >= 400 {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(string(body))
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// SetLang function (language setting for output functions)
 func setLang(lang string) {
 	if lang == "pl" {
 		l = langTexts["pl"]
